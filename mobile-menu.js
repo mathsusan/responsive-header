1aeb81bbb69b2dcd92f95306c94047dff372df00
Susan McKenzie <8696929+mathsusan@users.noreply.github.com>
2024-08-13 17:28:01 -0500

    update aria-hidden appropriately

--- mobile-menu.js
+++ mobile-menu.js
@@ -31,16 +31,24 @@ document.onreadystatechange = function () {
 function toggleFunction() {
   const navBar = document.getElementById('navbar');
   const toggleButton = document.getElementById('menu-toggle');
-  const navLinks = document.getElementById('nav-links');
-  const linkToFocus = navLinks.querySelector('a');
+  const navLinks = document.getElementById('nav-links').children;
+  const linkToFocus = navLinks[0].querySelector('a');
   if (navBar.classList.contains('open')) {
     toggleButton.setAttribute('aria-expanded', 'false');
     navBar.classList.remove('open');
-    navLinks.setAttribute('aria-hidden', true);
+    for (var i = 0; i < navLinks.length; i++) {
+      const link = navLinks[i].querySelector('a');
+      link.setAttribute('aria-hidden', 'true');
+      link.setAttribute('tab-index', '-1');
+    }
   } else {
     navBar.classList.add('open');
     toggleButton.setAttribute('aria-expanded', 'true');
     linkToFocus.focus();
-    navLinks.setAttribute('aria-hidden', false);
+    for (var i = 0; i < navLinks.length; i++) {
+      const link = navLinks[i].querySelector('a');
+      link.setAttribute('aria-hidden', 'false');
+      link.setAttribute('tab-index', '0');
+    }
   }
 }
