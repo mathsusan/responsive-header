808bd46cbfdc231b296c6a4f15d46f087d30ec31
Susan McKenzie <8696929+mathsusan@users.noreply.github.com>
2024-08-13 17:10:56 -0500

    Update to make this much better!

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,37 +1,46 @@
-function toggleFunction() {
-  var x = document.getElementById('header');
-  if (x.classList.contains('open')) {
-    x.classList.remove('open');
+function updateAriaHidden() {
+  const navLinks = document.getElementById('nav-links').children;
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 768) {
+    for (var i = 0; i < navLinks.length; i++) {
+      const link = navLinks[i].querySelector('a');
+      link.setAttribute('aria-hidden', 'true');
+      link.setAttribute('tab-index', '-1');
+    }
   } else {
-    x.classList.add('open');
+    for (var i = 0; i < navLinks.length; i++) {
+      const link = navLinks[i].querySelector('a');
+      link.setAttribute('aria-hidden', 'false');
+      link.setAttribute('tab-index', '0');
+    }
   }
 }
 
-const menulist = document.getElementsByClassName('menu-link');
-index = -1;
-
-document.addEventListener('keydown', (e) => {
-  //down key
-  if (e.key === 'ArrowDown') {
-    index++;
-    if (index === menulist.length) {
-      index = 0;
-    }
-    menulist[index].focus();
+// Add event listener for window resize
+window.addEventListener('resize', updateAriaHidden);
+// Call the function initially
+// window.addEventListener('load', updateAriaHidden());
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    updateAriaHidden();
+    console.log('Document is ready!');
   }
+};
 
-  //upkey
-  else if (e.key === 'ArrowUp') {
-    index--;
-    if (index === -1) {
-      index = menulist.length - 1;
-    }
-    menulist[index].focus();
+function toggleFunction() {
+  const navBar = document.getElementById('navbar');
+  const toggleButton = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+  const linkToFocus = navLinks.querySelector('a');
+  if (navBar.classList.contains('open')) {
+    toggleButton.setAttribute('aria-expanded', 'false');
+    navBar.classList.remove('open');
+    navLinks.setAttribute('aria-hidden', true);
+  } else {
+    navBar.classList.add('open');
+    toggleButton.setAttribute('aria-expanded', 'true');
+    linkToFocus.focus();
+    navLinks.setAttribute('aria-hidden', false);
   }
-
-  //Enter key
-  else if (e.key === 'Enter') {
-    console.log('Enter key was pressed ');
-    // menulist[index].click();
-  }
-});
+}
